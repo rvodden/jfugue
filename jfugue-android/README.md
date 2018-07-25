84c25e58008ccbbd2a88fe1427b1ab90875e88b9
Richard Vodden <richard@vodden.com>
2018-07-25 08:07:27 +0100

    Incorporated jfugue-android (although its omitted from the build)

---
+++ jfugue-android/README.md
@@ -0,0 +1,19 @@
+JFugue-for-Android
+==================
+
+[![Build Status](https://travis-ci.org/kshoji/JFugue-for-Android.svg?branch=master)](https://travis-ci.org/kshoji/JFugue-for-Android)
+
+Music library [JFugue](http://www.jfugue.org/) porting for Android<br />
+Based on the version 5.0.1.
+
+Usage
+=====
+
+See [the wiki page](https://github.com/kshoji/JFugue-for-Android/wiki) for configurations and using of this library.
+About the JFugue features, see [the original documents](http://www.jfugue.org/examples.html).
+
+License
+=======
+- JFugue 4.x for Android's license is same as the original, [LGPL](https://www.gnu.org/licenses/lgpl.html).
+- JFugue 5.x for Android's license is same as the original, [Apache 2.0 License](http://www.apache.org/licenses/LICENSE-2.0).
+- Other modified referential library's license is also same as original. See each directories' license file for more information.
---
+++ jfugue-android/src/main/java/org/staccato/DefaultNoteSettingsManager.java
@@ -0,0 +1,72 @@
+package org.staccato;
+
+import org.jfugue.midi.MidiDefaults;
+import org.jfugue.theory.Note;
+
+public class DefaultNoteSettingsManager {
+  public static final byte DEFAULT_DEFAULT_OCTAVE = 5;
+  public static final byte DEFAULT_DEFAULT_BASS_OCTAVE = 4; // Updated in JFugue5; in previous versions, bass was Octave 3
+  public static final double DEFAULT_DEFAULT_DURATION = 0.25d;
+  public static final byte DEFAULT_DEFAULT_ON_VELOCITY = MidiDefaults.MIDI_DEFAULT_ON_VELOCITY;
+  public static final byte DEFAULT_DEFAULT_OFF_VELOCITY = MidiDefaults.MIDI_DEFAULT_OFF_VELOCITY;
+  private static DefaultNoteSettingsManager instance;
+  private byte defaultOctave = DEFAULT_DEFAULT_OCTAVE;
+  private byte defaultBassOctave = DEFAULT_DEFAULT_BASS_OCTAVE;
+  private double defaultDuration = DEFAULT_DEFAULT_DURATION;
+  private byte defaultOnVelocity = MidiDefaults.MIDI_DEFAULT_ON_VELOCITY;
+  private byte defaultOffVelocity = MidiDefaults.MIDI_DEFAULT_OFF_VELOCITY;
+
+  private DefaultNoteSettingsManager() {
+  }
+
+  public static DefaultNoteSettingsManager getInstance() {
+    if (instance == null) {
+      instance = new DefaultNoteSettingsManager();
+    }
+    return instance;
+  }
+
+  public byte getDefaultOctave() {
+    return this.defaultOctave;
+  }
+
+  public void setDefaultOctave(byte octave) {
+    assert (octave < Note.MIN_OCTAVE) || (octave > Note.MAX_OCTAVE);
+    this.defaultOctave = octave;
+  }
+
+  public byte getDefaultBassOctave() {
+    return this.defaultBassOctave;
+  }
+
+  public void setDefaultBassOctave(byte octave) {
+    assert (octave < Note.MIN_OCTAVE) || (octave > Note.MAX_OCTAVE);
+    this.defaultBassOctave = octave;
+  }
+
+  public double getDefaultDuration() {
+    return this.defaultDuration;
+  }
+
+  public void setDefaultDuration(double duration) {
+    this.defaultDuration = duration;
+  }
+
+  public byte getDefaultOnVelocity() {
+    return this.defaultOnVelocity;
+  }
+
+  public void setDefaultOnVelocity(byte attack) {
+    assert (attack < MidiDefaults.MIN_ON_VELOCITY) || (attack > MidiDefaults.MAX_ON_VELOCITY);
+    this.defaultOnVelocity = attack;
+  }
+
+  public byte getDefaultOffVelocity() {
+    return this.defaultOffVelocity;
+  }
+
+  public void setDefaultOffVelocity(byte decay) {
+    assert (decay < MidiDefaults.MIN_OFF_VELOCITY) || (decay > MidiDefaults.MAX_OFF_VELOCITY);
+    this.defaultOffVelocity = decay;
+  }
+}
